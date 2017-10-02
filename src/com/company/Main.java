7d22c405b869615c8b86c56e21dde4f2b03c6a8c
Abushakhmin <abushakhmin.roman@gmail.com>
2017-10-02 20:11:59 +0300

    System.out.printf("Che kak?");

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,6 +3,7 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.printf("hi chuwak");
+        System.out.println("hi chuwak");
+        System.out.printf("Che kak?");
     }
 }
