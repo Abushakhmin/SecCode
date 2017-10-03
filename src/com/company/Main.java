866c789e892cb5a812b38692468d482fdf4ff524
Abushakhmin <abushakhmin.roman@gmail.com>
2017-10-03 20:32:58 +0300

    вывести через консоль с аргументами

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,7 +3,9 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("hi chuwak");
-        System.out.printf("Che kak?");
+        // Программа копирования файла из одной директории в другую
+        for (int i = 0; i < args.length; i++){
+            System.out.println(args[i]);
+        }
     }
 }
