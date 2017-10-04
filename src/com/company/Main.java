b5f48ca5298148c7291dceeb790bd80b5b30b13e
Abushakhmin <abushakhmin.roman@gmail.com>
2017-10-04 19:02:22 +0300

    Код по копированию файла из одной директории в другую через командную строку

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,11 +1,31 @@
 package com.company;
 
+import java.io.IOException;
+import java.io.InvalidClassException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.StandardCopyOption;
+
 public class Main {
 
     public static void main(String[] args) {
-        // Программа копирования файла из одной директории в другую
-        for (int i = 0; i < args.length; i++){
-            System.out.println(args[i]);
+        if(args.length != 2) {
+            System.out.println("Введите аргументы. Пример: /home/roman/workspace/test/first_forder/text /home/roman/workspace/test/second_forder/text1");
+            return;
+        }
+        try {
+            Path source = Paths.get(args[0]);
+            Path target = Paths.get(args[1]);
+            // скопировать файл
+            Files.copy(source, target, StandardCopyOption.REPLACE_EXISTING);
+            System.out.println("Выполнилось копирование. Но это не точно. Проверяй вручную пока");
+        }
+        catch (InvalidClassException e) {
+            System.out.println("Ошибка указания пути " + e);
+        }
+        catch (IOException e) {
+            System.out.println("Ошибка ввода вывода " + e);
         }
     }
 }
